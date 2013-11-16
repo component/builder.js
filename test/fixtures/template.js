3b12655251b7049a631e9fb1fdeab8816b1eabfb
Matthew Mueller <mattmuelle@gmail.com>
2013-11-16 15:05:53 -0800

    only build html, css and json templates. ignore every other file type in templates

--- test/fixtures/template.js
+++ test/fixtures/template.js
@@ -1,3 +1,9 @@
 require.register("templates/index.html", function(exports, require, module){
 module.exports = '<div></div>';
 });
+require.register("templates/index.json", function(exports, require, module){
+module.exports = '{\n  "name": "blah"\n}\n';
+});
+require.register("templates/index.css", function(exports, require, module){
+module.exports = 'h2 {\n  background: pink;\n}\n';
+});
